c369e6cb2e220aabafa6f7bbe32b16d386f10594
Sumit Kumar <sumit821044@gmail.com>
2025-08-30 15:38:23 +0530

    using hash in caching

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, jsonify
 import mysql.connector
 import redis
--- test_redis.py
+++ test_redis.py
@@ -7,14 +7,45 @@ load_dotenv()
 
 # Connect to Redis
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "127.0.0.1"),
+    host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     password=os.getenv("REDIS_PASSWORD")
 )
 
-# Set key with expiry (e.g., 60 seconds)
-r.setex("greeting", 60, "Hello, Redis world!")
+# # Set key with expiry (e.g., 60 seconds)
+# r.setex("greeting", 60, "Hello, Redis world!")
 
-# Get key back
-value = r.get("greeting")
-print(value.decode() if value else "Key expired or not found")
+# # Get key back
+# value = r.get("greeting")
+# print(value.decode() if value else "Key expired or not found")
+
+
+
+# Use pipeline - > avoid race conditions
+pipe = r.pipeline()
+
+# Create a hash + set expiry
+pipe.hset("user:2000", mapping={
+    "username": "eve",
+    "email": "eve@site.com",
+    "age": "28"
+})
+pipe.expire("user:2000", 60)
+
+# Read and update inside same pipeline
+pipe.hget("user:2000", "email")
+pipe.hgetall("user:2000")
+pipe.hincrby("user:2000", "age", 1)
+pipe.hget("user:2000", "age")
+
+# Execute all at once
+results = pipe.execute()
+
+print("Pipeline Results:")
+print("hset result:", results[0])  # number of fields added
+print("expire result:", results[1])  # True/False
+print("email:", results[2].decode())  # decode b'...'
+print("all fields:", {k.decode(): v.decode() for k, v in results[3].items()})
+print("age after incr:", results[4])  # integer
+print("age fetched again:", results[5].decode())
+print("(with expiry of 60 seconds)")
